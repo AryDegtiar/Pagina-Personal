4f22ca203d81f742d496b5f01785d33c78347459
AryDegtiar <adegtiar@frba.utn.edu.ar>
2022-12-29 02:40:46 -0300

    se agrego el apartado de conocimientos

---
+++ assests/js/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('scroll', function(){
+    let animationMachine = document.getElementsByClassName('machine');
+    let animationMachinePosition = animationMachine[0].getBoundingClientRect().top;
+    console.log(animationMachinePosition);
+    let screenPosition = window.innerHeight / 2;
+
+    if(animationMachinePosition < screenPosition){
+        for(let i = 0; i < animationMachine.length; i++){
+            var animacion = 'type' + i
+            animationMachine[i].classList.add(animacion);
+            console.log(animacion);
+        }
+    }else{
+        for(let i = 0; i < animationMachine.length; i++){
+            var animacion = 'type' + i
+            animationMachine[i].classList.remove(animacion);
+        }
+    }
+})
