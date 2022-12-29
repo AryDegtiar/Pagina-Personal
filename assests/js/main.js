812bfb88a2aee60c3146e614ef5ef06dd603de7b
AryDegtiar <adegtiar@frba.utn.edu.ar>
2022-12-29 13:03:36 -0300

    se cargaron imgs

--- assests/js/main.js
+++ assests/js/main.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function(){
     let animationMachine = document.getElementsByClassName('machine');
     let animationMachinePosition = animationMachine[0].getBoundingClientRect().top;
     console.log(animationMachinePosition);
-    let screenPosition = window.innerHeight / 2;
+    let screenPosition = window.innerHeight / 0.9;
 
     if(animationMachinePosition < screenPosition){
         for(let i = 0; i < animationMachine.length; i++){
